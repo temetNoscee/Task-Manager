3046a07b5d3d52853ecc4afbea0b7b26ab6f4e49
temetNoscee <piatesla09@gmail.com>
2023-11-14 11:21:15 +0300

    login and sign up interface completed

--- client/assets/login.js
+++ client/assets/login.js
@@ -2,6 +2,11 @@ const popup = document.querySelector(".popup");
 const loginNav = document.querySelector(".loginNav")
 const signupNav = document.querySelector(".signupNav");
 
+const btnPop = document.querySelector("#btnLoginNav");
+const closeBtn = document.querySelector(".closeBtn");
+
+const main = document.querySelector(".main");
+
 signupNav.addEventListener('click', ()=>{
     popup.classList.add('active');
 })
@@ -10,4 +15,15 @@ loginNav.addEventListener('click',()=>{
     popup.classList.remove("active");
 })
 
+btnPop.addEventListener('click',()=>{
+    popup.classList.add('active-popup');
+    main.style.display = 'none';
+    popup.style.display = 'flex';
+})
 
+closeBtn.addEventListener('click', ()=>{
+    popup.classList.remove('active-popup');
+    main.style.display='flex';
+    popup.style.display='none';
+
+})
